c35d470efb19fc39cde42009fb950836195461d6
Luke Schleder <luke.schleder@gmail.com>
2019-11-03 12:58:19 -0600

    improve form validation

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { HashRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 
 
@@ -11,38 +12,44 @@ class Comments extends Component {
     state = {
         input: '',
     }
-    
-    
+
+
     inputChangeHandler = (event) => {
         this.setState({
             ...this.state,
             input: event.target.value
         });
     }
-    
+
     compileFeedback = () => {
         this.props.dispatch({ type: "COMMENT_INFORMATION", payload: this.state.input });
         console.log(this.props);
-        this. props.history.push('/review');
+        this.props.history.push('/review');
     }
 
     render() {
         return (
-                <div className="Comments">
-                    <header className="Comments-header">
-                        <h1 className="Comments-Title">Comments</h1>
-                        <form onSubmit={this.compileFeedback}>
-                        {/* <input id="commentInput"type="text" onChange={this.inputChangeHandler}></input> */}
-                        <textarea id="commentInput"type="text" 
-                        onChange={this.inputChangeHandler} name="message" rows="5" cols="30" 
-                        placeholder="Say a few words about how your day went."></textarea>
-                        
-                        <Button variant="contained"onClick={this.compileFeedback} type= "submit">Next</Button>
-                        </form>
-                        
-                    </header>
-                    <br />
-                </div>
+            <div className="Comments">
+                <header className="Comments-header">
+                    <h1 className="Comments-Title">Comments</h1>
+                    <form onSubmit={this.compileFeedback}>
+                        <TextField
+                            onChange={this.inputChangeHandler}
+                            label="Comments"
+                            multiline
+                            rows="5"
+                            defaultValue=""
+                            margin="normal"
+                            variant="outlined"
+                            placeholder="Say a few words about how your day went."
+                        />
+                        <br/>
+                        <Button variant="contained" onClick={this.compileFeedback} type="submit">Next</Button>
+                    </form>
+
+                </header>
+                <br />
+            </div>
         );
     }
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -13,7 +13,7 @@ import Select from '@material-ui/core/Select';
 class Form extends Component {
 
     state = {
-        input: 1,
+        input: '',
     }
 
 
@@ -25,7 +25,12 @@ class Form extends Component {
     }
 
     compileFeedback = () => {
-        this.props.dispatch({ type: this.props.type, payload: this.state.input });
+        if (this.state.input === '' || this.state.input < 1 || this.state.input > 5) {
+            alert('Invalid input, please select a number 1-5');
+        } else {
+            this.props.dispatch({ type: this.props.type, payload: this.state.input });
+            this.props.history.push(this.props.route);
+        }
     }
 
     render() {
@@ -34,13 +39,10 @@ class Form extends Component {
                 <header className="Home-header">
                     <h1 className="Feeling-title">{this.props.question}</h1>
                     <FormControl>
-                        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
-                        {this.props.label}
+                        <InputLabel shrink id="shrinkin">
+                            {this.props.label}
                         </InputLabel>
                         <Select onChange={this.inputChangeHandler}>
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
                             <MenuItem value={1}>1</MenuItem>
                             <MenuItem value={2}>2</MenuItem>
                             <MenuItem value={3}>3</MenuItem>
@@ -48,8 +50,9 @@ class Form extends Component {
                             <MenuItem value={5}>5</MenuItem>
                         </Select>
                         <FormHelperText>Required</FormHelperText>
-                        <br/>
-                        <Link to={this.props.route}> <Button variant="contained" type="button" onClick={this.compileFeedback}>Next</Button> </Link>
+                        <br />
+                        <br />
+                        <Button variant="contained" type="button" onClick={this.compileFeedback}>Next</Button>
                     </FormControl>
                 </header>
                 <br />
@@ -62,4 +65,4 @@ class Form extends Component {
 
 const mapReduxStateToProps = state => state;
 
-export default connect(mapReduxStateToProps)(Form);
+export default connect(mapReduxStateToProps)(withRouter(Form));
